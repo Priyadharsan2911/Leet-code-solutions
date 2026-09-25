9754287720c8aa552c2ede96ec65b7e6e1154fff
Priyadharsan2911 <dharsan2915@gmail.com>
2026-09-25 09:08:38 +0530

    Time: 48 ms (30.5%), Space: 19.2 MB (70.44%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -2,14 +2,14 @@ class Solution:
     def rotatedDigits(self, n: int) -> int:
         invalid_digits = {'3', '4', '7'}
         diff_digits = {'2', '5', '6', '9'}
-        
         count = 0
+
         for i in range(1, n + 1):
             s = str(i)
             # A number is valid & changed if:
-            # 1. It contains NO invalid digits (3, 4, 7)
-            # 2. It contains AT LEAST ONE digit that changes upon rotation (2, 5, 6, 9)
+            # 1. Contains NO invalid digits (3, 4, 7)
+            # 2. Contains AT LEAST ONE digit that changes upon rotation (2, 5, 6, 9)
             if not any(c in invalid_digits for c in s) and any(c in diff_digits for c in s):
                 count += 1
-                
+
         return count
